35d2e57d38e735d00162c54c2c9b50dfa732a70d
Yichen <chenlin7789@gmail.com>
2023-07-26 21:06:00 -0700

    fixed bug: when a news has no image, return nothing

--- world-news-app/src/NewsListEntry.jsx
+++ world-news-app/src/NewsListEntry.jsx
@@ -5,7 +5,13 @@ const NewsListEntry = ({ news }) => {
   return (
     <div className='news-list-entry'>
       <a href={news.getAttribute('href')}>
-        <img className='thumbimg' src={news.getElementsByClassName('thumbimg')[0].getAttribute('src')}/>
+        <img className='thumbimg'
+          src={
+            news.getElementsByClassName('thumbimg').length !== 0 ?
+            news.getElementsByClassName('thumbimg')[0].getAttribute('src') :
+            null
+          }
+          />
         <span className='article-title'>{news.getElementsByClassName('arttitle')[0].innerHTML}</span><br/>
         <span className='source-info'>{news.getElementsByClassName('sourceinfo')[0].textContent.split('(')[0].trim('')}</span>
       </a>

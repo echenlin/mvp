791421107a1b810a6806a9fec3356be9cce58ab2
Yichen <chenlin7789@gmail.com>
2023-08-01 02:20:30 -0700

    edited the color/layout of globe

--- world-news-app/src/MyGlobe.jsx
+++ world-news-app/src/MyGlobe.jsx
@@ -4,6 +4,7 @@ import { useState, useEffect } from 'react';
 
 const MyGlobe = ({ handleClick }) => {
   const [countries, setCountries] = useState({ features: []});
+  const [hoverD, setHoverD] = useState();
 
   useEffect(() => {
     // load data
@@ -11,19 +12,23 @@ const MyGlobe = ({ handleClick }) => {
       return res.json()}).then(setCountries);
   }, []);
 
-
   return (
     <Globe
       width='700'
       height='600'
-      // backgroundColor='white'
-      // globeImageUrl="//unpkg.com/three-globe/example/img/earth-night.jpg"
-      // backgroundImageUrl="//unpkg.com/three-globe/example/img/night-sky.png"
-      backgroundColor='rgb(0,0,0,0)'
+      backgroundColor="rgba(0,0,0,0)"
+      showGlobe={false}
+      showAtmosphere={false}
       polygonsData={countries.features}
-      polygonCapColor	={() => `#${Math.round(Math.random() * Math.pow(2, 24)).toString(16).padStart(1, '0')}`}
+      polygonAltitude={d => d === hoverD ? 0.12 : 0.06}
+      // polygonCapColor={d => d === hoverD ? 'white' : 'lavender'}
+      polygonCapColor={d => d === hoverD ? 'white' : 'lightsteelblue'}
+      polygonSideColor={() => "rgba(0,0,0,0)"}
+      polygonStrokeColor={() => '#111'}
       polygonLabel={({ properties: d }) => `<b>${d.ADMIN}</b>`}
       onPolygonClick={handleClick}
+      onPolygonHover={setHoverD}
+      polygonsTransitionDuration={30}
     />
   )
 };
